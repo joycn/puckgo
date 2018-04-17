a40465b1d0eeee54f5b263c4c34bdb64f0745daf
joy <tyouicn@gmail.com>
2018-04-17 14:25:01 +0800

    add pprof
    Signed-off-by: joy <tyouicn@gmail.com>

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/joycn/puckgo/cmd"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
 	cmd.Execute()
 }
