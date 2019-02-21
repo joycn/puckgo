d4a544bc5238a1af4769465942af5d48785e177d
joy <tyouicn@gmail.com>
2019-02-21 19:51:22 +0800

    add build.sh
    Signed-off-by: joy <tyouicn@gmail.com>

--- build.sh
+++ build.sh
@@ -2,18 +2,23 @@ TYPE="rpm"
 PACKAGE_NAME="puckgo"
 VERSION="1.0.0"
 VENDOR="puck"
-BUILD_PATH=/tmp/puckgo
+#BUILD_PATH=/tmp/puckgo
+BUILD_PATH=package
+SCRIPTS_PATH=./scripts
 BUILD_BIN_PATH=$BUILD_PATH/usr/sbin
-BUILD_ETC_PATH=$BUILD_PATH/usr/etc/
+BUILD_ETC_PATH=$BUILD_PATH/usr/etc/puckgo/
+BUILD_LOG_PATH=$BUILD_PATH/var/log/puckgo/
+BUILD_SYSTEMD_PATH=$BUILD_PATH/usr/lib/systemd/system/
 
-rm -fr $BUILD_PATH
-mkdir $BUILD_PATH
-mkdir -p $BUILD_BIN_PATH
-mkdir -p $BUILD_ETC_PATH
+#rm -fr $BUILD_PATH
+#mkdir $BUILD_PATH
+#mkdir -p $BUILD_BIN_PATH
+#mkdir -p $BUILD_ETC_PATH
+#mkdir -p $BUILD_LOG_PATH
+#mkdir -p $BUILD_SYSTEMD_PATH
 
 go build -ldflags "-w -s"
 cp puckgo $BUILD_BIN_PATH
-cp puckgo.yml $BUILD_ETC_PATH
 
 #chmod 700 ${BUILD_PATH}/*
 fpm -f -s dir \
@@ -25,7 +30,6 @@ fpm -f -s dir \
     --prefix / \
     --vendor $VENDOR \
     --deb-no-default-config-files \
-    #--pre-install $SCRIPTS_PATH/pre-install \
     #--post-install $SCRIPTS_PATH/post-install \
     #--pre-uninstall $SCRIPTS_PATH/pre-uninstall \
     #--post-uninstall $SCRIPTS_PATH/post-uninstall
